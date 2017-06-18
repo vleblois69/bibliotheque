01cacba34163e4bda9aa6bdc9ea729f470991845
Vincent LEBLOIS <Vincent LEBLOIS>
2017-06-18 17:59:22 +0200

    Remplacement liste statique par lecture en base + Ajout possibilité d'ajouter un livre

--- scripts/accueil.js
+++ scripts/accueil.js
@@ -2,24 +2,49 @@
 
 var express = require('express');
 var cons = require('consolidate'); 
+var bodyParser = require('body-parser');
 var MongoClient = require('mongodb').MongoClient;
 
 //Fin des inclusions / import des modules
 
 var app = express(); // Création du serveur express
+app.use(bodyParser())
+
+var listeLivres; //Tableau qui va contenir tous les livres
 
 app.engine('html',cons.pug);
 app.set('view engine','html');
 app.set('views', __dirname + '/../views');
 
-app.listen(8000);
-console.log("Serveur express démarré sur le port 8000!");
-
 app.get('/books', function(req,res) //Fonction executée quand arrive sur la page localhost:8000, soit la racine
 {
-	var livre1 = {titre:"Halo",auteur:"Bungie",ISBN:1326497581236,dateAchat:"21/10/2016",etat:"Neuf",theme:"Science fiction"};
-	var livre2 = {titre:"The Witcher",auteur:"Blabla",ISBN:9423167589462,dateAchat:"03/11/2014",etat:"Bon etat",theme:"Aventure"};
-	var liste = [livre1,livre2];
-	res.render("index",{'liste':liste});
+	app.db.collection("livres").find().toArray(function(err, result)
+	{
+		if (err) throw err;
+		
+		listeLivres = Array.from(result);
+	});
+	res.render("index",{'liste':listeLivres});
 });
+
+app.post('/books/new', function(req,res,next) //Fonction executée quand l'utilisateur appuye sur le bouton du formulaire de création
+{
+	var livre = {'ISBN': req.body.isbn, 'titre': req.body.titre, 'auteur': req.body.auteur, 'dateAchat': req.body.dateAchat, 'etat': req.body.etat, 'theme': req.body.theme}; //Un instancie un objet avec en passant aux attributs les valeurs des champs
+	app.db.collection("livres").insert(livre, null, function(error, result){
+		if (error) throw error;
+		
+		console.log("Le livre a bien été ajouté !");
+		listeLivres.push(livre);//On ajoute le livre créé dans la liste
+		res.render("index",{'liste':listeLivres});
+	});
+});
+
+MongoClient.connect('mongodb://127.0.0.1/bibliotheque', function(error, db)// Connection à la BD mongo et association a l'application
+{
+	if (error) throw error;
+	app.db = db;
+	app.listen(8000);
+	console.log("Serveur express démarré sur le port 8000!");
+});
+
 
